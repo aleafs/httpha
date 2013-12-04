19020c1161e92862cadc7544efb15216c16083a5
aleafs <zhangxc83@gmail.com>
2013-12-04 10:15:45 +0800

    重构更新

--- test/httpha.test.js
+++ test/httpha.test.js
@@ -10,136 +10,88 @@ var httpha = require('../');
 
 describe('httpha interface', function () {
 
-  var onlines = {};
-  var servers = [];
+  it('should_basic_round_robin_works_fine', function (done) {
 
-  var _kfirst = function (o) {
-    for (var i in o) {
-      return i;
-    }
-  }
-
-  var _handle = function (req, res) {
-    res.writeHead(onlines[this.address().port]);
-    res.end('hello world');
-  };
-
-  /* {{{ private function before() */
-  before(function (done) {
-    var N = 2;
-    var next = function () {
-      if (0 === (--N)) {
-        done();
-      }
-    };
-
-    onlines = {};
-    servers = [];
-    for (var i = 0; i < N; i++) {
-      var S = http.createServer(_handle);
-      S.listen(0, function (err) {
-        should.ok(!err);
-        onlines[this.address().port] = 200;
-        next();
-      });
-      servers.push(S);
-    }
-  });
-  /* }}} */
-
-  /* {{{ private function after() */
-  after(function (done) {
-    servers.forEach(function (s) {
-      s.close();
-    });
-    servers = [];
-    onlines = {};
-    done();
-  });
-  /* }}} */
-
-  /* {{{ should_basic_api_works_fine() */
-  it('should_basic_api_works_fine', function (done) {
+    var map = {'online' : true, 'offline' : true};
     var _me = httpha.create({
-      'mininterval' : 10,
-      'maxinterval' : 50,
-    });
-
-    Object.keys(onlines).forEach(function (p) {
-      _me.addserver('127.0.0.1', p);
-    });
-
-    var res = {};
-    for (var i = 0; i < 100; i++) {
-      var p = _me.fetch().port;
-      if (!res[p]) {
-        res[p] = 1;
-      } else {
-        res[p]++;
-      }
-    }
-
-    Object.keys(onlines).forEach(function (p) {
-      res[p].should.eql(50);
-    });
-
-    onlines[_kfirst(onlines)] = 302;
-    setTimeout(function () {
-      var res = {};
-      for (var i = 0; i < 100; i++) {
-        var p = _me.fetch().port;
-        if (!res[p]) {
-          res[p] = 1;
-        } else {
-          res[p]++;
-        }
-      }
-
-      should.not.exists(res[_kfirst(onlines)]);
-      Object.keys(onlines).forEach(function (p) {
-        if (p !== _kfirst(onlines)) {
-          res[p].should.eql(100);
-        }
+      'interval' : 10,
+    }, function (one, callback) {
+      process.nextTick(function () {
+        callback(null, map[one]);
       });
-
-      done();
-    }, 100);
-  });
-  /* }}} */
-
-  /* {{{ should_all_server_done_works_fine() */
-  it('should_all_server_done_works_fine', function () {
-    var _me = httpha.create({
-      'mininterval' : 10,
-      'maxinterval' : 50,
-      'statusurl' : 'if_online',
     });
 
     (function () {
       _me.fetch();
     }).should.throw('EmptyServerListException');
 
-    Object.keys(onlines).forEach(function (p) {
-      onlines[p] = 404;
-      _me.addserver('127.0.0.1', p);
-    });
+    _me.add('online');
+    _me.add('offline');
 
-    var res = {};
+    var res = {'online' : 0, 'offline' : 0};
     for (var i = 0; i < 100; i++) {
-      var p = _me.fetch().port;
-      if (!res[p]) {
-        res[p] = 1;
-      } else {
-        res[p]++;
-      }
+      res[_me.fetch()]++;
     }
-
-    Object.keys(onlines).forEach(function (p) {
-      res[p].should.eql(50);
+    res.should.eql({
+      'online' : 50, 'offline' : 50,
     });
 
+    map.offline = false;
+    setTimeout(function () {
+      var res = {'online' : 0, 'offline' : 0};
+      for (var i = 0; i < 100; i++) {
+        res[_me.fetch()]++;
+      }
+      res.should.eql({
+        'online' : 100, 'offline' : 0,
+      });
+      done();
+    }, 30);
   });
-  /* }}} */
 
+});
+
+describe('http status checker', function () {
+
+  var S = http.createServer(function (req, res) {
+
+    if ('/timeout' === req.url) {
+      setTimeout(function () {
+        res.end('timeout');
+      }, 100);
+    } 
+    else if ('/404' === req.url) {
+      res.writeHead(404);
+      res.end();
+    }
+    else {
+      res.writeHead(200);
+      res.end();
+    }
+  });
+
+  var P = null;
+  before(function(done) {
+    S.listen(0, function (err) {
+      should.ok(!err);
+      P = this.address().port;
+      done();
+    });
+  });
+
+  after(function () {
+    P = null;
+    S.close();
+  });
+
+  it('should_http_status_checker_works_fine', function (done) {
+
+    var _fn = httpha.httpStatusChecker('status', 20);
+    _fn({'hostname' : '127.0.0.1', 'port' : P}, function (err, yes) {
+      should.ok(!err);
+      yes.should.eql(true);
+      done();
+    });
+  });
 });
 

6df8b0cf4ecc40dbb3b143b6d020ed5cab5ad82f
aleafs <zhangxc83@gmail.com>
2013-12-04 11:11:51 +0800

    test case complete

--- test/httpha.test.js
+++ test/httpha.test.js
@@ -10,6 +10,7 @@ var httpha = require('../');
 
 describe('httpha interface', function () {
 
+  /* {{{ should_basic_round_robin_works_fine() */
   it('should_basic_round_robin_works_fine', function (done) {
 
     var map = {'online' : true, 'offline' : true};
@@ -28,23 +29,48 @@ describe('httpha interface', function () {
     _me.add('online');
     _me.add('offline');
 
-    var res = {'online' : 0, 'offline' : 0};
-    for (var i = 0; i < 100; i++) {
-      res[_me.fetch()]++;
-    }
-    res.should.eql({
-      'online' : 50, 'offline' : 50,
-    });
-
-    map.offline = false;
     setTimeout(function () {
       var res = {'online' : 0, 'offline' : 0};
       for (var i = 0; i < 100; i++) {
         res[_me.fetch()]++;
       }
       res.should.eql({
-        'online' : 100, 'offline' : 0,
+        'online' : 50, 'offline' : 50,
       });
+
+      map.offline = false;
+      setTimeout(function () {
+        var res = {'online' : 0, 'offline' : 0};
+        for (var i = 0; i < 100; i++) {
+          res[_me.fetch()]++;
+        }
+        res.should.eql({
+          'online' : 100, 'offline' : 0,
+        });
+        done();
+      }, 100);
+    }, 30);
+  });
+  /* }}} */
+
+  it('should_all_server_done_works_fine', function (done) {
+    var map = {'online' : false, 'offline' : false};
+    var _me = httpha.create({
+      'interval' : [10, 0],
+    }, function (one, callback) {
+      process.nextTick(function () {
+        callback(null, map[one]);
+      });
+    });
+
+    _me.add('online');
+    _me.add('offline');
+
+    _me.fetch().should.eql('online');
+    _me.fetch().should.eql('offline');
+    setTimeout(function () {
+      _me.fetch().should.eql('online');
+      _me.fetch().should.eql('offline');
       done();
     }, 30);
   });
@@ -58,7 +84,7 @@ describe('http status checker', function () {
     if ('/timeout' === req.url) {
       setTimeout(function () {
         res.end('timeout');
-      }, 100);
+      }, 30);
     } 
     else if ('/404' === req.url) {
       res.writeHead(404);
